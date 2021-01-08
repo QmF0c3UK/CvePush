1bebbb1609761d23bfb3df95945f4988d939f665
QmF0c3UK <QmF0c3UK@users.noreply.github.com>
2021-01-08 10:35:33 +0800

    Update cvealert.py

--- cvealert.py
+++ cvealert.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+#-*- coding:utf-8 -*-
 banner = """
         888888ba             dP                     
         88    `8b            88                     
@@ -7,15 +7,13 @@ banner = """
         88    .88 88.  .88   88         88 88.  .88 
         88888888P `88888P8   dP   `88888P' `88888P' 
    ooooooooooooooooooooooooooooooooooooooooooooooooooooo 
-                @time:2020/12/30 attac_push.py
+                @time:2021/01/07 cvealert.py
                 C0de by NebulabdSec - @batsu                  
  """
 print(banner)
-
 import configparser
 import smtplib
 from email.mime.text import MIMEText
-from translate import Translator
 import random
 import datetime
 import httpx
@@ -24,7 +22,7 @@ import requests
 import json
 
 config_dict = {}
-
+data_list = []  # 准备存取数据
 proxies = {'http': "http://127.0.0.1:8080",
            'https': "http://127.0.0.1:8001"
            }
@@ -59,107 +57,87 @@ headers = {
 }
 
 
-def get_config():
+def get_config(mail_setting):
     try:
         config = configparser.ConfigParser()
-        config.read("config.ini",encoding='UTF-8')
+        config.read("config.ini", encoding='UTF-8')
         data = {
-            'smtp_server': config.get('Mail_config', 'smtp_server'),
-            'smtp_port': config.get('Mail_config', 'smtp_port'),
-            'mail_addr': config.get('Mail_config', 'mail_addr'),
-            'auth_code': config.get('Mail_config', 'auth_code'),
-            'mail_to': config.get('Mail_config', 'mail_to'),
-            'Recipient': config.get('Mail_config', 'Recipient').split(','),
+            'mail_setting': int(mail_setting),
             'care': int(config.get('Config', 'care')),
             'vul_like': config.get('Config', 'vul_like').split(','),
             'risk_like': config.get('Config', 'risk_like').split(',')
-            # 'cpid':config.get('Server_config', 'cpid'),
-            # 'cpsecret': config.get('Server_config', 'cpsecret')
         }
         config_dict.update(data)
+        if mail_setting == 0:#更新微信企业的key值
+            data = {'key': config.get('Wxqy_config', 'key')}
+            config_dict.update(data)
+        elif mail_setting == 1:# 更新邮件推送的值
+            data = {
+                'smtp_server': config.get('Mail_config', 'smtp_server'),
+                'smtp_port': config.get('Mail_config', 'smtp_port'),
+                'mail_addr': config.get('Mail_config', 'mail_addr'),
+                'auth_code': config.get('Mail_config', 'auth_code'),
+                'mail_to': config.get('Mail_config', 'mail_to'),
+                'Recipient': config.get('Mail_config', 'Recipient').split(',')
+            }
+            config_dict.update(data)
         # print(config_dict)
     except Exception as e:
-        exit('[获取配置文件失败:%s]' % str(e))
+        exit('[+]获取配置文件失败:%s' % str(e))
+
 
 def get_cve():
     # 时间比我们晚，#取三天前的漏洞才有cvss评分
     today = datetime.date.today()
-    yesterday = today - datetime.timedelta(days=3)  # 3
-    pubStartDate = str(yesterday) + 'T00:00:00:000 UTC-05:00'
-    print(pubStartDate)
+    pubStartDate = str(today - datetime.timedelta(days=3)) + 'T00:00:00:000 UTC-05:00'
+    pubEndtDate = str(today - datetime.timedelta(days=2)) + 'T00:00:00:000 UTC-05:00'
+    params = {
+        'pubStartDate': pubStartDate,
+        'pubEndDate': pubEndtDate,
+        'resultsPerPage': 2000
+    }
     try:
-        if config_dict["care"] == 1:
-            for risk in config_dict["risk_like"]:
-                params = {'pubStartDate': pubStartDate, 'cvssV3Severity': risk}
-                # with httpx.Client(headers=headers, params=params, proxies=proxies) as client:
-                with httpx.Client(headers=headers, params=params, verify=False) as client:
-                # res = httpx.Client(headers=headers, params=params, proxies=proxies).get(url).json()
-                    res = client.get(url).json()
-                    if res['totalResults'] > 0:
-                        message = "[+] %s号共有`%s`个%s漏洞\n<font color=\"info\"><br>下面是漏洞简介-></font><br>\n" % (
-                            time.strftime("%Y-%m-%d", time.localtime()), str(res['totalResults']), risk)
-                        res_Formatted_output(res, message)
-        elif config_dict["care"] == 0:
+        if config_dict["care"] == 0:
             for risk in config_dict["risk_like"]:
                 for vul in config_dict["vul_like"]:
-                    params = {'pubStartDate': pubStartDate, 'keyword': vul, 'cvssV3Severity': risk}
+                    params.update({'cvssV3Severity': risk, 'keyword': vul})
                     with httpx.Client(headers=headers, params=params, verify=False) as client:
                         res = client.get(url).json()
-                        if res['totalResults'] > 0:
-                            message = "[+] %s号%s共有`%s`个%s漏洞\n<font color=\"info\"><br>下面是漏洞简介-></font><br>\n" % (
-                                time.strftime("%Y-%m-%d", time.localtime()), vul, str(res['totalResults']), risk)
-                            res_Formatted_output(res, message)
-        else:
-            print("[+]今天没发现新的CVE")
+                        print(res['totalResults'])
+                        insert_data(risk, res)
+        elif config_dict["care"] == 1:
+            for risk in config_dict["risk_like"]:
+                params.update({'cvssV3Severity': risk})
+                with httpx.Client(headers=headers, params=params, verify=False) as client:
+                    res = client.get(url).json()
+                    print(res['totalResults'])
+                    insert_data(risk, res)
+
+        # print(data_list)
     except Exception as e:
-        print("[-]1、报错了兄弟:%s" % e)
+        print("[-]1、兄弟,获取cve漏洞的时候报错了:%s" % e)
+        get_cve()
 
-
-def res_Formatted_output(res, message):
-    print(message)
-    global baseSeverity, score
+def insert_data(risk,res):
     for i in range(res['totalResults']):
-        # print(i)
-        id = '漏洞编号：%s' % res['result']['CVE_Items'][i]['cve']['CVE_data_meta']['ID']
-        pubdate = '公开日期：%s' % res['result']['CVE_Items'][i]['publishedDate'][0:10]
-        # reference_data = '漏洞参考链接：<a href="%s">漏洞来源</a>' % \
-                         # str(res['result']['CVE_Items'][i]['cve']['references']['reference_data'][0]['url'])
-        reference_data = '漏洞参考链接：[漏洞来源](%s)' % \
-                         str(res['result']['CVE_Items'][i]['cve']['references']['reference_data'][0]['url'])
-        try:
-            baseSeverity = '<font color="warning">漏洞等级：%s</font>' % \
-                           res['result']['CVE_Items'][i]['impact']['baseMetricV3']['cvssV3']['baseSeverity']
-            score = '<font color="warning">CVSSV3：%s</font>' % str(res['result']['CVE_Items'][i]['impact']['baseMetricV3']['cvssV3']['baseScore'])
-            print(baseSeverity,score)
-        finally:
-            print(i)
-            description = res['result']['CVE_Items'][i]['cve']['description']['description_data'][0]['value']
-            # description = translat(description)  # 谷歌翻译
-            print(description)
-            description = tranlate(description)#其他翻译
-            description = '漏洞描述：<font color=\"info\">' + description + '</font>'
-            content = '**【新增漏洞告警】**\n%s\n%s\n%s\n%s\n%s\n%s' % (
-            id, pubdate, baseSeverity, score, description, reference_data)
-            try:
-                send_wx(content)
-            except Exception as e:
-                print("[-]2、报错了兄弟:%s" % e)
-        continue
+        cve_url = ''
+        for j in range(len(res['result']['CVE_Items'][i]['cve']['references']['reference_data'])):
+            cve_url =cve_url + "[漏洞参考链接%s](%s)\n" % (j, str(res['result']['CVE_Items'][i]['cve']['references']['reference_data'][j]['url']))
+        requests_data = {
+            'risk': risk,
+            'CVE编号': res['result']['CVE_Items'][i]['cve']['CVE_data_meta']['ID'],
+            '公开时间': res['result']['CVE_Items'][i]['publishedDate'][0:10],
+            '漏洞描述': tranlate(res['result']['CVE_Items'][i]['cve']['description']['description_data'][0]['value']),
+            '漏洞参考链接': cve_url
+        }
+        data_list.append(requests_data)
 
-
-# def translat(context):  # 翻译描述信息
-#     if len(context) >= 500:
-#         return context
-#     else:
-#         translator = Translator(service_urls=['translate.google.com', ], to_lang="chinese", proxies=proxies)
-#         translation = translator.translate(context)
-#         return translation
 
 def tranlate(context):
     try:
         direction = "auto2zh"
         tranlate_url = "http://api.interpreter.caiyunai.com/v1/translator"
-        token = "3975l6lr5pcbvidl6jl2"
+        token = "3975l6lr5pcbvidl6jl2"#建议使用自己的apikey
         payload = {
             "source": context,
             "trans_type": direction,
@@ -172,17 +150,27 @@ def tranlate(context):
         }
         response = requests.request("POST", tranlate_url, data=json.dumps(payload), headers=headers)
         return json.loads(response.text)['target']
-    except:
-        # print(context)
-        return context
-    # text = json.loads(response.text)['target']
-    # return text
+    except Exception as e:
+        print("[-]2、兄弟,翻译报错了:%s" % e)
+        time.sleep(5)
+        tranlate(context)
+        # return context
 
-def send_mail(data):
-    message = MIMEText(data, 'html', 'utf-8')
+
+def deal_content(content):
+    message = "**%s漏洞**" %  datetime.date.today()
+    for data in content:
+
+        message = message + "<font color='warning'>[%s]</font>%s\n<font color='info'>%s</font>\n%s" \
+                  % (data['risk'], data['CVE编号'], data['漏洞描述'], data['漏洞参考链接'])
+    return message
+
+def send_mail(content):
+    message = MIMEText(content, 'html', 'utf-8')
     message['From'] = config_dict["mail_addr"]
     message['To'] = config_dict["mail_to"]
     message['Subject'] = '最新CVE漏洞'
+
     try:
         stmp = smtplib.SMTP_SSL(config_dict["smtp_server"], config_dict["smtp_port"])
         stmp.login(config_dict["mail_addr"], config_dict["auth_code"])
@@ -198,22 +186,42 @@ def send_mail(data):
 
 
 def send_wx(content):
-    wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=********-****-****-****-0*********7"
+    message = ''
+    wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + config_dict['key'] +"&debug=1"
+    message = deal_content(content)
+    print(len(message))
+    # print(message)
     data = {"msgtype": "markdown",
             "markdown": {
-                "content": content
+            "content": message
             }}
+    # print(data)
     res = httpx.post(wx_url, headers=headers, json=data).json()
     errcode = res['errcode']
     if errcode == 0:
         print('[+]发送成功')
     else:
-        print('something wrong:\nyou can see: https://open.work.weixin.qq.com/devtool/query?e=' + str(errcode))
+        print('something wrong:\nyou can see:%s' % res)
+        # print('something wrong:\nyou can see: https://open.work.weixin.qq.com/devtool/query?e=' + str(errcode))
+
 
 
 def main():
-    get_cve()
+    mail_setting = int(input("漏洞爬取默认是当天最新全部的CVE漏洞，如需更改请修改config文件。\n0、微信企业账户\n1、邮箱发送\n请输入想要发送数据的方式:"))
+    # mail_setting = 0
+    if mail_setting == 0:
+        get_config(mail_setting)
+        get_cve()
+        send_wx(data_list)
+    elif mail_setting == 1:
+        get_config(mail_setting)
+        get_cve()
+        print(data_list)
+    else:
+        print("[+]兄弟你这个输入是错误的，不符合规范，请重新输入")
 
 if __name__ == '__main__':
-    get_config()
-    main()
+    while 1:
+        data_list = []  # 初始化
+        main()
+        time.sleep(86400)
